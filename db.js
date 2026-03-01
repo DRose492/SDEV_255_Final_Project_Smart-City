d479deeaf688a0b8e9f52e909da12c5589bc1265
DRose492 <146631485+DRose492@users.noreply.github.com>
2026-03-01 14:58:58 -0600

    Delete db.js

--- db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://smartcity:smartcity255@smartcityfinalproject.nicvwbc.mongodb.net/?appName=smartcityfinalproject")
-
-module.exports = mongoose
